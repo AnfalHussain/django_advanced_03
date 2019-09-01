b9b1baa4daa1e21dd135726e9b4068a933e85456
AnfalHussain <anfalae95@gmail.com>
2019-09-01 12:03:29 +0300

    Anfal & Wadha

--- stores/admin.py
+++ stores/admin.py
@@ -2,4 +2,21 @@ from django.contrib import admin
 from .models import Store
 
 # Register your models here.
-admin.site.register(Store)
+
+class StoreAdmin(admin.ModelAdmin):
+    # ...Store
+    list_display = ('id', 'name','description',)
+    list_filter = ['added']
+    search_fields = ['description','name', ]
+    list_editable = ['description',]
+    list_display_links = ['name',]
+
+
+
+
+
+
+
+
+
+admin.site.register(Store, StoreAdmin)
--- stores/models.py
+++ stores/models.py
@@ -6,5 +6,9 @@ class Store(models.Model):
     description = models.TextField()
     added = models.DateTimeField(auto_now_add=True)
 
+
+
     def __str__(self):
         return self.name
+
+
